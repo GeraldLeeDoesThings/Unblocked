a6ae38db876fa4c87e9dfdd734293def02fbbb30
Maxarino <maxwellborgeszulauf@gmail.com>
2022-01-23 00:55:01 -0500

    Update graph visualizer to use graph.py

--- visualizer.py
+++ visualizer.py
@@ -1,34 +1,26 @@
-import networkx as nx
-import matplotlib.pyplot as plt
+from graph import *
 import json
 
-
-class Graph:
-
-    def __init__(self):
-        self.edges = []
-
-    def add_edge(self, fr, to, weight):
-        self.edges.append([fr, to, weight])
-
-    def visualize(self):
-        g = nx.DiGraph()
-        for fr, to, weight in self.edges[:10]:
-            g.add_edge(fr, to, weight=weight)
-        nx.draw_networkx(g)
-        plt.show()
-
-
-g = Graph()
+transactions = set()
 
 with open("data.json", "r") as file:
     data = json.load(file)
+
     for _, block in data.items():
-        for transaction in json.loads(block)['transactions']:
-            g.add_edge(
-                transaction['from'],
-                transaction['to'],
-                float(transaction['value'])/(10**18)
+        block = json.loads(block)
+        for transaction in block['transactions']:
+            sender = Wallet(transaction['from'])
+            recipient = Wallet(transaction['to'])
+            transactions.add(
+                Transaction(
+                    sender,
+                    recipient,
+                    float(transaction['value'])/(10**18),
+                    transaction['nonce'],
+                    block['number']
+                )
             )
+
+g = Graph([], transactions)
 
 g.visualize()
